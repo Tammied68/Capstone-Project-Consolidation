207add17889a494310849bb1a74ce28e77805d8f
Tammie <tammied68@yahoo.com>
2026-05-26 23:54:12 -0500

    Restore complete news application functionality

--- news_project/urls.py
+++ news_project/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
+    path("", include("news.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
-
 
 """
 My main project URL. URL configuration for news_project project.
